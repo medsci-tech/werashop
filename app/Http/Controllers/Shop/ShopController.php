e6f84244fde23493880cef6341eba0f4748a59a2
蔡乾 <cai.q@foxmail.com>
2016-02-16 09:30:08 +0800

    add shopcontroller

--- app/Http/Controllers/Shop/ShopController.php
+++ app/Http/Controllers/Shop/ShopController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Shop;
 
+use App\Models\Commodity;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
@@ -9,5 +10,15 @@ use App\Http\Controllers\Controller;
 
 class ShopController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('shop.index');
+    }
+
+    public function category()
+    {
+        return view('shop.category')->with([
+            'items' => Commodity::all()
+        ]);
+    }
 }
